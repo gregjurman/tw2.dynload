ad6ba7a8aaf223bfd3b09c51ca1bc75bf121cce8
Greg Jurman <gdj2214@rit.edu>
2012-02-23 14:23:56 -0500

    Added test_require to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+# Ridiculous as it may seem, we need to import multiprocessing and
+# logging here in order to get tests to pass smoothly on python 2.7.
+# Thanks RJ!
+import multiprocessing
+import logging
+
 setup(
     name='tw2.dynload',
     version='2.0b1',
@@ -11,13 +17,24 @@ setup(
         "tw2.core",
         "tw2.jquery",
         ## Add other requirements here
-        "Genshi",
+        "mako",
         ],
     packages=find_packages(exclude=['ez_setup', 'tests']),
     namespace_packages = ['tw2'],
     zip_safe=False,
     include_package_data=True,
     test_suite = 'nose.collector',
+    tests_require = [
+        'nose',
+        'BeautifulSoup',
+        'Genshi',
+        'mako',
+        # formencode isn't actually needed, but is just here to patch up
+        # tw2.forms,
+        'formencode',
+        'strainer',
+        'WebTest'
+    ],
     entry_points="""
         [tw2.widgets]
         # Register your widgets so they can be listed in the WidgetBrowser
